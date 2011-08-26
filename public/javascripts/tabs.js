4799321b391ccacafbcf43d155e7abfd80007c6f
mark <mhurwitzus@gmail.com>
2011-08-26 10:11:15 -0500

    redid js links so scripts load properly, features page works as does pricing page with animations

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,18 +1,18 @@
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
 
 	//Default Action
-	$(".tab_content").hide(); //Hide all content
-	$("#tour-tab-links li:first").addClass("active").show(); //Activate first tab
-	$(".tab_content:first").show(); //Show first tab content
+	jQuery(".tab_content").hide(); //Hide all content
+	jQuery("#tour-tab-links li:first").addClass("active").show(); //Activate first tab
+	jQuery(".tab_content:first").show(); //Show first tab content
 
 	//On Click Event
-	$("#tour-tab-links li").click(function() {
-		$("#tour-tab-links li").removeClass("active"); //Remove any "active" class
-		$(this).addClass("active"); //Add "active" class to selected tab
-		$(".tab_content").hide(); //Hide all tab content
-		var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
-		$(activeTab).fadeIn(); //Fade in the active content
+	jQuery("#tour-tab-links li").click(function() {
+		jQuery("#tour-tab-links li").removeClass("active"); //Remove any "active" class
+		jQuery(this).addClass("active"); //Add "active" class to selected tab
+		jQuery(".tab_content").hide(); //Hide all tab content
+		var activeTab = jQuery(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+		jQuery(activeTab).fadeIn(); //Fade in the active content
 		return false;
 	});
 
